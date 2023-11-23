4b064c233344c5db419317dcf8845f1ffe270e9b
MxHassan <mxhassan195@gmail.com>
2023-11-23 04:55:48 +0200

    api finished but without error handling and jwt

--- index.js
+++ index.js
@@ -4,6 +4,25 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts");
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL).then(() => {
+  console.log("Connected to MongoDB");
+});
+    
+//middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+//routes
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/posts", postRoute);
 
 app.listen(5000, () => {
   console.log("server is running on 5000");
